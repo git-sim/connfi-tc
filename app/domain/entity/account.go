2bdb9dcc14e62c2ef39f16b263ede03f8fc44987
Simran Nanda <simran76@gmail.com>
2019-12-01 15:55:34 -0700

    Added first entity

---
+++ app/domain/entity/account.go
@@ -0,0 +1,50 @@
+package entity
+
+// Account represents an messaging account. Email should be unique
+type AccountID_t int64
+type Account struct {
+	id        AccountID_t
+	email     string    
+	firstName string
+	lastName  string
+}
+
+//How to create a new account verifying the email is unique
+// We need a collection of existing accounts.
+// When creating a new account need to check that the email is unique
+// When created the account will get assigned an inbox (collection of inbox messages)
+// Inbox could start empty or with initial welcome message (from admin).
+
+
+// NewAccount instantiates a new Account. 
+func NewAccount(newid AccountID_t, newemail string) *Account {
+	return &Account {
+		id:        newid,
+		email:     newemail,
+	}
+}
+
+func (a *Account) GetID() AccountID_t {
+	return a.id
+}
+
+func (a *Account) GetEmail() string {
+	return a.email
+}
+
+func (a *Account) GetFirstName() string {
+	return a.firstName
+}
+
+func (a *Account) GetLastName() string {
+	return a.lastName
+}
+
+// NewAccounts instanties a slice of new Accounts
+func NewAccounts(Emails ...string) []*Account {
+	var as []*Account
+	for i, email := range Emails {
+		as = append(as, NewAccount(AccountID_t(i+1), email))
+	}
+	return as
+}
---
+++ app/domain/entity/account_test.go
@@ -0,0 +1,32 @@
+package entity
+
+import (
+	"testing"
+    "reflect"
+)
+
+func TestNewAccount(t *testing.T) {
+	a := &Account{
+		id:        1,
+		email:     "Alice.Smith@mail.com",
+	}
+	if !reflect.DeepEqual(a, NewAccount(a.id, a.email)) {
+		t.Error("expected equal structs")
+	}
+}
+
+func TestNewAccounts(t *testing.T) {
+	a := []*Account{
+		{
+			id:        1,
+			email:     "Alice.Smith@mail.com",
+		},
+		{
+			id:        2,
+			email:     "BobSmith@mail.com",
+		},
+	}
+	if !reflect.DeepEqual(a, NewAccounts(a[0].email, a[1].email)) {
+		t.Error("expected equal structs")
+	}
+}
