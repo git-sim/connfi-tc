95590a88c1dc25b3df7b5f3b34050f08ee86099a
Simran Nanda <simran76@gmail.com>
2020-01-04 20:54:03 -0700

    regen'd with the create-react-app, and added src files

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+// import the Container Component from the semantic-ui-react
+import { Container } from "semantic-ui-react";
+import Top from "./Top";
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <Top />        
+      </Container>
+    </div>
+  );
+}
+export default App;
